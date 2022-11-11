a1b6248aec93b812f1fef5447c55419be11478e4
bernkastel15 <redpangilinan715@gmail.com>
2022-11-12 05:52:04 +0800

    Update functionality

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,21 @@
-//Advice Slip API
-const random_advice_api = "https://api.adviceslip.com/advice";
+const api = "https://api.adviceslip.com/advice";
+const advice_txt = document.body.querySelector('#advice');
+const advice_id = document.body.querySelector('#advice_id');
 
-//Variables
+window.onload = () => {
+    getAdvice();
+}
+
+function getAdvice() {
+    try {
+        fetch(api).then(response => {
+            return response.json();
+        }).then(advData => {
+            const advObj = advData.slip;
+            advice_txt.innerHTML = `${advObj.advice}`;
+            advice_id.innerHTML = `${advObj.id}`;
+        })
+    } catch (err) {
+        console.error(err);
+    }
+}
